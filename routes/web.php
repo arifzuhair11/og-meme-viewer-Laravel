0122663e97dfd2d050e6f4f3698ae458159cbee9
arifzuhair11 <arifzuhair11@gmail.com>
2019-11-23 19:24:26 +0800

    added to web route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/memes/all', 'MemesController@index')->name('memes.index');
+Route::get('/memes/page/{page}', 'MemesController@setByPage')->name('memes.bypage');
+Route::post('/memes', 'MemesController@store')->name('memes.store');
+Route::get('/memes/{memes}', 'MemesController@show')->name('memes.show');
+Route::get('/popular', 'MemesController@popular')->name('memes.popular');
